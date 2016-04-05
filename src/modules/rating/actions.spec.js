638c8b21487aac304c3c1c4157f70e4ad2ffbb76
Nikolay Kozhukharenko <nikolay.kozhuharenko@gmail.com>
2016-04-05 21:22:18 +0300

    tests for rating actions

---
+++ src/modules/rating/actions.spec.js
@@ -0,0 +1,73 @@
+'use strict';
+
+import chai  from 'chai';
+import configureStore from 'redux-mock-store';
+import { loadSpecoffersChooser, loadRatingList, highlighEnrolment } from './actions'
+import {REQUEST_API} from '../../system/constants';
+import * as types from './constants';
+
+let expect = chai.expect;
+
+describe('Rating actions', () => {
+  const mockStore = configureStore();
+
+  describe('loadSpecoffersChooser AC', () => {
+    let initialData = {
+      rating: {
+        specofferChooser: {
+          resources: [1,2,3]
+        }
+      }
+    };
+    const interrupt = loadSpecoffersChooser().interrupt;
+
+    it('interrupt for loadSpecoffersChooser should return true ' +
+        'when data fro specoffeShoose is available (full array)', () => {
+      let store = mockStore(initialData);
+      expect(interrupt(store)).to.equal(true)
+    });
+
+    it('interrupt for loadSpecoffersChooser should return false ' +
+        'when data for specoffer choose is not available (empty)', () => {
+      initialData.rating.specofferChooser.resources = [];
+      let store = mockStore(initialData);
+      expect(interrupt(store)).to.equal(false)
+    });
+
+  });
+
+  describe('loadRatingList, highlighEnrolment ACs', () => {
+    
+    it('should create an action to load rating list ', () => {
+      const specofferId = 1;
+
+      const expectedAction = {
+        type: REQUEST_API,
+        request: {
+          url: `/enrolments/1/ratings`,
+          actions: {
+            start: {type: types.LOAD_RATING_LIST_START},
+            success: {type: types.LOAD_RATING_LIST_SUCCESS},
+            fail: {type: types.LOAD_RATING_LIST_FAIL}
+          }
+        }
+      };
+
+      expect(loadRatingList(specofferId)).to.eql(expectedAction)
+    });
+
+    it('should create an action to highligh an enrolment ', () => {
+      const enrolmentId = 1;
+
+      const expectedAction = {
+        type: types.HIGHLIGH_ENROLMENT,
+        payload: {
+          enrolmentId
+        }
+      };
+
+      expect(highlighEnrolment(enrolmentId)).to.eql(expectedAction)
+    })
+
+  })
+});

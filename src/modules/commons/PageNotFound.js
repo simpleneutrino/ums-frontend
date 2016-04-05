659cb9dc8c604ead1d3ffff01c69ce2d714c09e3
simpleneutrino <bandicoot39@gmail.com>
2016-04-05 22:01:17 +0300

    add 404 and helpers styles file

--- src/modules/commons/PageNotFound.js
+++ src/modules/commons/PageNotFound.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router';
 const PageNotFound = React.createClass({
   render: function() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1 style={{textAlign: 'center'}}>
+      <div className='center-align'>
+        <h1 className='center-align'>
           404 сторінка не знайдена
         </h1>
         <Link className="btn btn-lg btn-success" to="/" >Перейти на гловну сторінку</Link>
